2ccb0d349907fe0f9c95c7f1ff95edb9d2427d86
Rajkumar <rajkumarbecse59@gmail.com>
2017-05-30 21:50:20 +0530

    Added code

---
+++ UploadMultipleFilesInMVC/Controllers/MultipleController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadMultipleFilesInMVC.Controllers
+{
+    public class MultipleController : Controller
+    {
+        // GET: Multiple
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/UploadedFiles/"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
